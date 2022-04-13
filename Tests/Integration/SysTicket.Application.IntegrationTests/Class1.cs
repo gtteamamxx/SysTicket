2aef1210899d2b598455ed458558573ccf3347e3
Grzegorz Stanik <g.stanik@o2.pl>
2022-04-13 19:04:58 +0200

    ReportGenerator

---
+++ Tests/Integration/SysTicket.Application.IntegrationTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SysTicket.Application.IntegrationTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
