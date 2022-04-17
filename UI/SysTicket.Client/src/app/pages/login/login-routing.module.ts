4ea30b812d778441b53b1077a968e6459b778779
Grzegorz Stanik <g.stanik@o2.pl>
2022-04-17 12:32:29 +0200

    Bug 88: Możliwość cofnięcia się do widoku logowania po wcześniejszym zalogowaniu

--- UI/SysTicket.Client/src/app/pages/login/login-routing.module.ts
+++ UI/SysTicket.Client/src/app/pages/login/login-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { SysTicketRoutes } from 'src/app/core/models/common/systticket-routes.model';
 import { LoginComponent } from './login.component';
+import { LoginGuard } from './login.guard';
 
 const routes: SysTicketRoutes = [
   {
@@ -9,12 +10,14 @@ const routes: SysTicketRoutes = [
     data: {
       title: 'Logowanie',
     },
+    canActivate: [LoginGuard],
     component: LoginComponent,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
+  providers: [LoginGuard],
   exports: [RouterModule],
 })
 export class LoginRoutingModule { }
---
+++ UI/SysTicket.Client/src/app/pages/login/login.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Store } from "@ngxs/store";
+import { Observable } from "rxjs";
+import { NavigationService } from "src/app/core/services/navigation.service";
+import { UserState } from "src/app/core/store/user.state";
+
+@Injectable()
+export class LoginGuard implements CanActivate {
+    constructor(
+        private readonly navigationService: NavigationService,
+        private readonly store: Store) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        if (this.store.selectSnapshot(UserState.isUserLogged)) {
+            this.navigationService.navigateToMainPage();
+            return false;
+        }
+
+        return true;
+    }
+}
