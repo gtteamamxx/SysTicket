0ba5b96ec0b1070d3d2bb3e61b752e4b0c7d68e1
Grzegorz Stanik <g.stanik@o2.pl>
2022-05-02 20:19:52 +0200

    Task 62: Testy jednostkowe

---
+++ Application/SysTicket.Application/ProjectInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("SysTicket.Application.IntegrationTests")]
+[assembly: InternalsVisibleTo("SysTicket.Application.UnitTests")]
+[assembly: InternalsVisibleTo("SysTicket.Infrastructure.IntegrationTests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+namespace SysTicket.Application
+{
+    public class ProjectInfo
+    {
+    }
+}
